22c16c671b887f352240928b048280385b81b8d3
unknown <siddharthkotak280@gmail.com>
2025-07-25 01:23:43 +0530

    background job

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
 
+"use client";
 import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-import { caller } from "@/trpc/server";
-const Page = async () =>{
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+const Page = () =>{
+   
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({
+    onSuccess: () => {
+      toast.success("Back job started")
+    }
+  }))
+
     return(
-      <div>
-        
+      <div className="p-4 max-w-7xl mx-auto">
+        <Button disabled={invoke.isPending} onClick={()=>invoke.mutate({text:"Test"})}>
+          Invoke Background job
+        </Button>
       </div>
     )    
 }
